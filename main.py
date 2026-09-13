dd3f60afceec2f15afa58b8940834c8ba8b3bcbf
Кирилл Соловьев <k.solovev994@yandex.ru>
2026-09-13 22:16:16 +0300

    fix functions

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ instrument_name = "Скрипка"
 cost = 15999.99
 is_booked = True
 
-def get_booking_status(is_booked):
+def add_instrument(instrument_name, price):
+    return f"Инструмент {instrument_name} добавлен по цене {price}"
+
+def change_price(instrument_name, new_price):
+    return f"Цена {instrument_name} изменена на {new_price}"
+
+def change_booking_status(instrument_name, is_booked):
     if is_booked:
-        return "Инструмент забронирован для покупки"
-    return "Инструмент готов к покупке"
+        is_booked = False
+        return f"Инструмент {instrument_name} готов к покупке"
+    is_booked = True
+    return f"Инструмент {instrument_name} забронирован для покупки"
 
 print(f"Инструмент: {instrument_name}")
 print(f"Стоимость: {cost} рублей (Наличными: {math.floor(cost)})")
-print(get_booking_status(is_booked))
+print(change_booking_status(instrument_name, is_booked))
